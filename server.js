4fb29daf2154adbc74ac84533905295c51912c63
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-16 04:43:56 -0400

    added code for database connection

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const figlet = require("figlet");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12341234",
+    database: "company_db",
+  });
